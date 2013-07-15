ec00945b7cecc24a237c084c3905fc27c4dca278
tombazza <tombazza@gmail.com>
2013-07-15 09:32:05 +0100

    Updated javascript formatting

--- js/gush.js
+++ js/gush.js
@@ -17,51 +17,52 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-
 var settings = {
     dataEndpoint: 'data.php',
     numberEngines: 3
 };
 
-var $Gush = function($, $Config) {
-	var searchTable = false,
-		openRow = false,
-		infoRow = false,
-		resultsIndex = [],
-		loading = 0,
-		error = false,
-		errorTypes = ['Generic', 'Data', 'Auth'],
-		requests = [];
-	
-	function init() {
-		$('#query').keydown(formSubmit);
-		$('#query').focus(formFocus);
-		$('#query').blur(formBlur);
-		infoRow = $('.info_content').clone();
-	}
-	
-	function formFocus(e) {
-		if(!connectionManager.hasAuth()) {
-			$(this).removeClass('passcode').val('').attr('type', 'password');
-		}
-	}
-	function formBlur(e) {
-		if(!connectionManager.hasAuth()) {
-			$(this).val('').attr('type', 'text').val('passcode?').addClass('passcode');
-		}
-	}
-	function formSubmit(e) {
-		if (e.which == 13) {
-			if(!connectionManager.hasAuth()) {
+var $Gush = function ($, $Config) {
+    var searchTable = false,
+        openRow = false,
+        infoRow = false,
+        resultsIndex = [],
+        loading = 0,
+        error = false,
+        errorTypes = ['Generic', 'Data', 'Auth'],
+        requests = [];
+
+    function init() {
+        $('#query').keydown(formSubmit);
+        $('#query').focus(formFocus);
+        $('#query').blur(formBlur);
+        infoRow = $('.info_content').clone();
+    }
+
+    function formFocus(e) {
+        if (!connectionManager.hasAuth()) {
+            $(this).removeClass('passcode').val('').attr('type', 'password');
+        }
+    }
+
+    function formBlur(e) {
+        if (!connectionManager.hasAuth()) {
+            $(this).val('').attr('type', 'text').val('passcode?').addClass('passcode');
+        }
+    }
+
+    function formSubmit(e) {
+        if (e.which == 13) {
+            if (!connectionManager.hasAuth()) {
                 $(this).addClass('loading');
-				connectionManager.performLogin($(this).val(), postAuth);
-			} else {
+                connectionManager.performLogin($(this).val(), postAuth);
+            } else {
                 performSearch();
-				
-			}
-		}
-	}
- 
+
+            }
+        }
+    }
+
     function performSearch() {
         error = false;
         $('body').removeClass('error');
@@ -71,24 +72,24 @@ var $Gush = function($, $Config) {
         $('#query').addClass('loading');
         connectionManager.submitSearch(query, searchResponse);
     }
-    
-    var connectionManager = function() {
+
+    var connectionManager = function () {
         var requests = [],
             passcode = '',
             authenticated = false;
-    
+
         function performLogin(code, callback) {
             passcode = $.trim(code);
             loadData({
                 a: 'auth',
                 p: passcode
-            }, function(data) {
+            }, function (data) {
                 handleAuthResponse(data, callback);
             });
         }
-        
+
         function submitSearch(query, callback) {
-            for(i = 0; i < $Config.numberEngines; i++) {
+            for (i = 0; i < $Config.numberEngines; i++) {
                 loadData({
                     a: 'search',
                     e: i,
@@ -97,7 +98,7 @@ var $Gush = function($, $Config) {
                 }, callback);
             }
         }
-        
+
         function getMetadata(id, engine, callback) {
             loadData({
                 a: 'metadata',
@@ -106,7 +107,7 @@ var $Gush = function($, $Config) {
                 p: passcode
             }, callback);
         }
-        
+
         function loadData(post, callback) {
             var requestId = requests.length + 1;
             requests[requestId] = $.ajax({
@@ -114,11 +115,11 @@ var $Gush = function($, $Config) {
                 url: $Config.dataEndpoint,
                 data: post,
                 dataType: 'json',
-                success: function(data) {
+                success: function (data) {
                     deleteRequest(requestId);
-                    if(checkErrorState(data)) callback(data);
+                    if (checkErrorState(data)) callback(data);
                 },
-                error: function(xhr, status, error) {
+                error: function (xhr, status, error) {
                     deleteRequest(requestId);
                     displayError({
                         message: 'Request failed',
@@ -127,38 +128,38 @@ var $Gush = function($, $Config) {
                 }
             });
         }
-        
+
         function deleteRequest(requestId) {
             var position = $.inArray(requestId, requests);
-            if(~position) requests.splice(position, 1);
+            if (~position) requests.splice(position, 1);
         }
-        
+
         function checkErrorState(data) {
-            if(data.error) {
-				displayError(data.error);
+            if (data.error) {
+                displayError(data.error);
                 return false;
             }
             return true;
         }
-        
+
         function handleAuthResponse(data, callback) {
-            if(!checkErrorState) return;
-            if(data.auth) {
+            if (!checkErrorState) return;
+            if (data.auth) {
                 authenticated = true;
                 callback();
             }
         }
-        
+
         function terminateRequests() {
-            $.each(requests, function(id, xhr) {
-                if(xhr) xhr.abort();
+            $.each(requests, function (id, xhr) {
+                if (xhr) xhr.abort();
             });
         }
-    
+
         function getAuthenticated() {
             return authenticated;
         }
-        
+
         return {
             performLogin: performLogin,
             submitSearch: submitSearch,
@@ -167,13 +168,14 @@ var $Gush = function($, $Config) {
             hasAuth: getAuthenticated
         };
     }();
-	
-	
-    
+
+
+
     /**
      * Executed in the event of a successful authentication
      * @returns null
      */
+
     function postAuth() {
         $('body').removeClass('load');
         $('#query').removeClass('loading').attr('type', 'text').val('');
@@ -183,111 +185,125 @@ var $Gush = function($, $Config) {
                 'dtJs': '//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/jquery.dataTables.min.js'
             },
             callback: {
-                'dtJs': function(url, result, key) {
+                'dtJs': function (url, result, key) {
                     jQuery.extend(jQuery.fn.dataTableExt.oSort, {
-                        "file-size-pre": function( a ) {
-                            var x = a.substring(0,a.length - 2);
+                        "file-size-pre": function (a) {
+                            var x = a.substring(0, a.length - 2);
                             var x_unit = (a.substring(a.length - 2, a.length) == "MB" ? 1000 : (a.substring(a.length - 2, a.length) == "GB" ? 1000000 : 1));
-                            return parseInt( x * x_unit, 10 );
+                            return parseInt(x * x_unit, 10);
                         },
-                        "file-size-asc": function( a, b ) {return ((a < b) ? -1 : ((a > b) ? 1 : 0));},
-                        "file-size-desc": function( a, b ) {return ((a < b) ? 1 : ((a > b) ? -1 : 0));}
+                        "file-size-asc": function (a, b) {
+                            return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+                        },
+                        "file-size-desc": function (a, b) {
+                            return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+                        }
                     });
                 }
             }
         });
     }
-	
-	function displayError(response) {
-		var query = $('#query');
-		console.log(response);
-		$('body').removeClass('load').addClass('error');
-		query.attr('type', 'text').val('Error: ' + response.message);
-		
-		if(response.code == 2) {
-			query.attr('disabled', 'disabled').blur();
+
+    function displayError(response) {
+        var query = $('#query');
+        console.log(response);
+        $('body').removeClass('load').addClass('error');
+        query.attr('type', 'text').val('Error: ' + response.message);
+
+        if (response.code == 2) {
+            query.attr('disabled', 'disabled').blur();
             connectionManager.stopAll();
-			setTimeout(function() {window.location = window.location;}, 3000);
-		} else {
-			query.removeClass('loading');
-		}
+            setTimeout(function () {
+                window.location = window.location;
+            }, 3000);
+        } else {
+            query.removeClass('loading');
+        }
         error = true;
-		
-	}
-	function searchResponse(data) {
+
+    }
+
+    function searchResponse(data) {
         console.log(data);
-			loading--;
-			var tableData = {
-				bPaginate: false,
-				bFilter: false,
-				bInfo: false,
-				aaData: [],
-				aoColumns: [
-					{sTitle: 'Name', sWidth: '70%'},
-					{sTitle: 'Size', sType: 'file-size'},
-					{sTitle: 'Seeds'},
-					{sTitle: 'Peers'}
-				]
-			};
-			$.each(data, function (key, item) {
-				var hash = item.hash.toUpperCase();
-				if (typeof (resultsIndex[hash]) === 'undefined') {
-					resultsIndex[hash] = item;
-					resultsIndex[hash].metadata = [item.metadata];
-					tableData.aaData.push([
-							item.name,
-							item.size,
-							item.seeds,
-							item.peers,
-							hash
-					]);
-				} else {
-					// add trackers to list
-					var trackers = resultsIndex[hash].magnetParts.tr;
-					resultsIndex[hash].magnetParts.tr = trackers.concat(item.magnetParts.tr);
-					resultsIndex[hash].metadata.push(item.metadata); 
-				}
-			});
-			if (loading == 0) $('#query').removeClass('loading');
-			initTable(tableData);
-	}
-	function initTable(tableData) {
-		if (!searchTable) {
-			searchTable = $('#results').dataTable(tableData);
-		} else {
-			searchTable.fnAddData(tableData.aaData);
-		}
-		searchTable.fnSort([
-			[2, 'desc']
-		]);
-		$('#results tbody tr').unbind('click');
-		$('#results tbody tr').bind('click', function () {
-			if (searchTable.fnIsOpen(this)) {
-				searchTable.fnClose(this);
-			} else {
-				var info = searchTable.fnGetData(this)[4];
-				searchTable.fnClose(openRow);
-				searchTable.fnOpen(this, drawInfoRow(info), "info_row");
-				openRow = this;
-			}
-		});
-	}
-	function drawInfoRow(hash) {
-		var row = resultsIndex[hash];
-		console.log(row);
-		var magnetURI = 'magnet:?xt=' + row.magnetParts.xt[0] + '&tr=' + row.magnetParts.tr.join('&tr=') + '&dn=' + row.magnetParts.dn[0];
-		infoRow.find('a.magnet').attr('href', magnetURI);
-        
+        loading--;
+        var tableData = {
+            bPaginate: false,
+            bFilter: false,
+            bInfo: false,
+            aaData: [],
+            aoColumns: [{
+                sTitle: 'Name',
+                sWidth: '70%'
+            }, {
+                sTitle: 'Size',
+                sType: 'file-size'
+            }, {
+                sTitle: 'Seeds'
+            }, {
+                sTitle: 'Peers'
+            }]
+        };
+        $.each(data, function (key, item) {
+            var hash = item.hash.toUpperCase();
+            if (typeof (resultsIndex[hash]) === 'undefined') {
+                resultsIndex[hash] = item;
+                resultsIndex[hash].metadata = [item.metadata];
+                tableData.aaData.push([
+                    item.name,
+                    item.size,
+                    item.seeds,
+                    item.peers,
+                    hash
+                ]);
+            } else {
+                // add trackers to list
+                var trackers = resultsIndex[hash].magnetParts.tr;
+                resultsIndex[hash].magnetParts.tr = trackers.concat(item.magnetParts.tr);
+                resultsIndex[hash].metadata.push(item.metadata);
+            }
+        });
+        if (loading == 0) $('#query').removeClass('loading');
+        initTable(tableData);
+    }
+
+    function initTable(tableData) {
+        if (!searchTable) {
+            searchTable = $('#results').dataTable(tableData);
+        } else {
+            searchTable.fnAddData(tableData.aaData);
+        }
+        searchTable.fnSort([
+            [2, 'desc']
+        ]);
+        $('#results tbody tr').unbind('click');
+        $('#results tbody tr').bind('click', function () {
+            if (searchTable.fnIsOpen(this)) {
+                searchTable.fnClose(this);
+            } else {
+                var info = searchTable.fnGetData(this)[4];
+                searchTable.fnClose(openRow);
+                searchTable.fnOpen(this, drawInfoRow(info), "info_row");
+                openRow = this;
+            }
+        });
+    }
+
+    function drawInfoRow(hash) {
+        var row = resultsIndex[hash];
+        console.log(row);
+        var magnetURI = 'magnet:?xt=' + row.magnetParts.xt[0] + '&tr=' + row.magnetParts.tr.join('&tr=') + '&dn=' + row.magnetParts.dn[0];
+        infoRow.find('a.magnet').attr('href', magnetURI);
+
         var commentsText = 'Comments';
-        if(row.comments) {
+        if (row.comments) {
             commentsText = 'Comments (' + row.comments + ')';
         }
         infoRow.find('#comments-tab a').html(commentsText);
-        
+
         infoRow.find('#file-page table tbody').html('');
         infoRow.find('#comments-page').html('');
-        
-        infoRow.find('li a').click(function(e){
+
+        infoRow.find('li a').click(function (e) {
             e.preventDefault();
             infoRow.find('li').removeClass('selected');
             $(this).parent().addClass('selected');
@@ -295,34 +311,34 @@ var $Gush = function($, $Config) {
             infoRow.find('.tab-contents').hide();
             infoRow.find('#' + id + '-page').show();
         });
-        $.each(row.metadata, function(id, meta) {
+        $.each(row.metadata, function (id, meta) {
             connectionManager.getMeta(meta.id, meta.name, receiveMetaData);
         });
-        
-		return infoRow;
-	}
-    
+
+        return infoRow;
+    }
+
     function receiveMetaData(data) {
         console.log(data);
-        if(data.files.length) {
+        if (data.files.length) {
             filesPage = infoRow.find('#file-page table tbody');
             var fileList = '';
-            if(filesPage.html() == '') {
-                $.each(data.files, function(id, value){
+            if (filesPage.html() == '') {
+                $.each(data.files, function (id, value) {
                     fileList += '<tr><td>' + value.filename + '</td><td>' + value.size + '</td></tr>';
                 });
                 infoRow.find('#file-page table tbody').html(fileList);
             }
         }
-        if(data.comments) {
+        if (data.comments) {
             commentsPage = infoRow.find('#comments-page');
             commentCount = infoRow.find('#comments-tab a').html().match(/\d+\.?\d*/g);
             console.log(commentCount);
-            if(!commentCount) commentCount = 0;
+            if (!commentCount) commentCount = 0;
             var commentsHtml = commentsPage.html();
-            for(i = 0; i < data.comments.length; i++) {
+            for (i = 0; i < data.comments.length; i++) {
                 comment = $.trim(data.comments[i]);
-                if(comment) commentsHtml += '<div>' + comment + '</div>';
+                if (comment) commentsHtml += '<div>' + comment + '</div>';
                 commentCount++;
             }
             commentsPage.html(commentsHtml);
@@ -330,11 +346,11 @@ var $Gush = function($, $Config) {
         }
     }
 
-	var contract = {
-		init: init
-	};
-	
-	return contract;
+    var contract = {
+        init: init
+    };
+
+    return contract;
 }(jQuery, settings);
 
 
