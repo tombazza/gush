bd2afb752d2c8610cbd2058deaa4a877400de83e
tombazza <tombazza@gmail.com>
2015-12-16 14:17:36 +0000

    Removed version number

--- webroot/index.php
+++ webroot/index.php
@@ -2,7 +2,7 @@
 <!-- Gush | GPLv3 | http://git.io/gush -->
 <html>
 	<head>
-		<title>Gush 1.0</title>
+		<title>Gush</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
