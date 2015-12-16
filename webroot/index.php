017c4769c99eacff7e6cc61be6ea7c056a8d9f45
tombazza <tombazza@gmail.com>
2015-12-16 14:06:16 +0000

    Changed font to Open Sans

--- webroot/index.php
+++ webroot/index.php
@@ -5,6 +5,7 @@
 		<title>Gush 1.0</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 		<link href="css/normalize.css" rel="stylesheet" type="text/css">
 		<link href="css/gush.css" rel="stylesheet" type="text/css">
 		<link href="css/jquery.dataTables.css" rel="stylesheet" type="text/css">
